036a5fb6df4e397494c90f306edea687ba4bf809
Baráth Dávid <davidbarath08@gmail.com>
2022-04-28 08:58:51 +0200

    Adatbázis Kapcsolat

---
+++ src/index.php
@@ -0,0 +1,9 @@
+<?php
+//Kapcsolódás Adatbázishoz
+$con = new mysqli("localhost","kek","titok","kek");
+
+if($con->connect_error){
+    die("Hiba! A kapcsolódás sikertelen");
+}
+echo "ok";
+$con->close();
