b49d9a9155301b105e6f05a849cda542e7d837ef
Baráth Dávid <davidbarath08@gmail.com>
2022-04-28 08:49:52 +0200

    + SQL tartalom

---
+++ database/tartalom.sql
@@ -0,0 +1,9 @@
+insert into dolgozok
+--Milyen mezőket adunk meg
+(nev,telepules,fizetes)
+values
+("Tangó Béla","Szeged",385),
+("Tarka Márk","Esztergom",385),
+("Fakó Lajos","Szolnok",385),
+("Kopács Dorina","Budapest",385),
+("Monoki Gábor","Jászberény",385);
