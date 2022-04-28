75cb7a15449211cbe7c46f296a48824b69257aa1
Baráth Dávid <davidbarath08@gmail.com>
2022-04-28 09:14:46 +0200

    Lekeres müködik

--- src/index.php
+++ src/index.php
@@ -5,5 +5,21 @@ $con = new mysqli("localhost","kek","titok","kek");
 if($con->connect_error){
     die("Hiba! A kapcsolódás sikertelen");
 }
-echo "ok";
+//echo "ok";
+
+$sql = <<<EOT
+select * from dolgozok
+EOT;
+
+if($result = $con->query($sql)){//eltároljuk az adatokat,megnézzük hogy jó e az sql utasítás
+   // echo"lekeres ok";
+} else{
+    echo "nme ok";
+}
+
+while($row = $result->fetch_assoc()){ //fetch ->kiszedjük az adatok belőle->miylen módón->1sor 1 row
+echo $row["nev"];
+}
+
+
 $con->close();
